e8fcbdb2d8f99e6d9e6ee405f0d4d84cf4c8d45d
Bee Sight Soft <>
2018-05-28 15:42:21 +0700

    first commit

---
+++ Demo Collapsed view swift/ColViewCell/TestCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TestCollectionViewCell.swift
+//  Demo Collapsed view swift
+//
+//  Created by Bee Sight Soft on 5/28/18.
+//  Copyright © 2018 BeeSight Soft. All rights reserved.
+//
+
+import UIKit
+
+class TestCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Demo Collapsed view swiftTests/Demo_Collapsed_view_swiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Demo_Collapsed_view_swiftTests.swift
+//  Demo Collapsed view swiftTests
+//
+//  Created by Bee Sight Soft on 5/28/18.
+//  Copyright © 2018 BeeSight Soft. All rights reserved.
+//
+
+import XCTest
+@testable import Demo_Collapsed_view_swift
+
+class Demo_Collapsed_view_swiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
