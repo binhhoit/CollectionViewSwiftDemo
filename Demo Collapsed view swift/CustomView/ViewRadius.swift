f558d3c155fb3c720401666cc271049cd5ef3ee7
Bee Sight Soft <>
2018-05-29 08:48:35 +0700

    first commit 2

---
+++ Demo Collapsed view swift/CustomView/ViewRadius.swift
@@ -0,0 +1,9 @@
+//
+//  ViewRadius.swift
+//  Demo Collapsed view swift
+//
+//  Created by Bee Sight Soft on 5/28/18.
+//  Copyright © 2018 BeeSight Soft. All rights reserved.
+//
+
+import Foundation
